4767ec84536dcf615d0d7d715a0ee7578081c94e
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-07-29 21:46:06 +0530

    Exp-6H-Patern1.cpp

---
+++ Exp-6H-Patern1.cpp
@@ -0,0 +1,24 @@
+//Vinay Koli Exp-6
+//24070123133
+//B2
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    int j;
+    int n=5;
+    for(i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+/*
+Output:
+*
+**
+***
+****
+*****
+*/
