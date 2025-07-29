772f973ce292fc37031b12bdbfe5e31e666d4e6c
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-07-29 21:35:56 +0530

    Exp-6F-Hourglass-pattern.cpp

---
+++ Exp-6F-Hourglass-pattern.cpp
@@ -0,0 +1,38 @@
+// Vinay Koli Exp-7
+// 24070123133
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " "; 
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << "* "; 
+        }
+        cout << endl;
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " "; 
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+/*Output:
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+   * *
+  * * *
+ * * * *
+ * * * * *
+*/
