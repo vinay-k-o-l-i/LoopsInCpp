85c9837efba5aeda40dae089c5f3f10703ecddcf
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-07-29 21:23:34 +0530

    Exp-6B-ForLoopPrintSIT5Times.cpp

---
+++ Exp-6B-ForLoopPrintSIT5Times.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    string Name;
+    Name="sit";
+    for(i=0;i<5;i++){
+    cout<<Name<<"\n";
+    }
+    return 0;
+/*
+Output:
+sit
+sit
+sit
+sit
+sit
+*/
